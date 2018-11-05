c41a9970b33edb1110e14d56f518f71ab93cd520
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-05 14:46:39 +0100

    hidden attribute, showing hiding elements

--- src/app/playlists/playlist-details/playlist-details.component.ts
+++ src/app/playlists/playlist-details/playlist-details.component.ts
@@ -1,19 +1,39 @@
 import { Component, OnInit } from "@angular/core";
 import { Playlist } from "../../models/playlist";
 
+enum Modes {
+  show,
+  hide
+}
+
 @Component({
   selector: "app-playlist-details",
   templateUrl: "./playlist-details.component.html",
   styleUrls: ["./playlist-details.component.css"]
 })
 export class PlaylistDetailsComponent implements OnInit {
-  
   playlist: Playlist = {
     id: 123,
     name: "Angular Hits",
     favourite: true,
     color: "#ff00ff"
   };
+  // modes = Modes
+  // mode:Modes = Modes.show
+
+  mode: "show" | "edit" = "show";
+
+  edit() {
+    this.mode = "edit";
+  }
+
+  cancel() {
+    this.mode = "show";
+  }
+
+  save() {
+    console.log("save");
+  }
 
   constructor() {}
 
