7bd6e02bd57c2799c6c6eacb36f2f02374ff4dea
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-05 11:18:53 +0100

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,25 @@
-import { Component, ChangeDetectorRef, ChangeDetectionStrategy } from "@angular/core";
+import {
+  Component,
+  ChangeDetectorRef,
+  ChangeDetectionStrategy
+} from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ["./app.component.css"]
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-
   title = "Hello Angular";
 
   counter = 0;
 
   constructor(cdr: ChangeDetectorRef) {
-
-    setInterval(()=>{
-
-        this.counter++;
-
-        cdr.detectChanges()
-    },500)    
+    // setInterval(()=>{
+    //     this.counter++;
+    //     cdr.detectChanges()
+    // },500)
     // console.log(this)
   }
 }
