f2bea551f7c7b7b05566852d70f3c9b6cfc6b6ce
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-05 15:58:54 +0100

    ngFor ngForOf ngForOfContext, let implicit

--- src/app/playlists/items-list/items-list.component.ts
+++ src/app/playlists/items-list/items-list.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Playlist } from "../../models/playlist";
 
 @Component({
-  selector: 'app-items-list',
-  templateUrl: './items-list.component.html',
-  styleUrls: ['./items-list.component.css'],
+  selector: "app-items-list",
+  templateUrl: "./items-list.component.html",
+  styleUrls: ["./items-list.component.css"],
   encapsulation: ViewEncapsulation.Emulated
 })
 export class ItemsListComponent implements OnInit {
 
-  constructor() { }
+  playlist: Playlist[] = [
+    {
+      id: 123,
+      name: "Angular Hits",
+      favourite: true,
+      color: "#ff00ff"
+    },
+    {
+      id: 234,
+      name: "Best of Angular ",
+      favourite: false,
+      color: "#00ffff"
+    },
+    {
+      id: 345,
+      name: "Angular Top 20",
+      favourite: true,
+      color: "#ffff00"
+    }
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
