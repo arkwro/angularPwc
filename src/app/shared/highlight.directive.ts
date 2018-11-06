3200d5eda7220310a74eb2948b12c316868c3a30
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-06 12:33:08 +0100

    Directive host bindings

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -9,7 +9,11 @@ import {
 } from "@angular/core";
 
 @Directive({
-  selector: "[appHighlight]"
+  selector: "[appHighlight]",
+  // host:{
+  //   '[style.borderLeftColor]':'appHighlight',
+  //   '(mouseenter)':'activate($event)'
+  // }
 })
 export class HighlightDirective implements OnChanges, DoCheck {
   @Input()
@@ -18,16 +22,19 @@ export class HighlightDirective implements OnChanges, DoCheck {
   constructor(private elem: ElementRef, private renderer: Renderer2) {}
 
   ngOnChanges(changes:SimpleChanges) {
-    console.log("ngOnChange",changes);
+    // console.log("ngOnChange",changes);
     this.renderer.setStyle(this.elem.nativeElement, "color", this.appHighlight);
   }
   
   ngDoCheck() {
-    console.log("ngDoCheck");
+    // console.log("ngDoCheck");
   }
 
   ngOnInit() {
-    console.log("hello", this.appHighlight);
+    // console.log("hello", this.appHighlight);
+  }
+
+  ngOnDestroy(){
 
   }
 }
