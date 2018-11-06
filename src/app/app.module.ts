374e0403f228623a8748757e772ca5ef193af37c
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-06 14:40:49 +0100

    appUnless - directive with asterisk  microsyntax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule, ApplicationRef } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PlaylistsModule } from "./playlists/playlists.module";
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [AppComponent],
