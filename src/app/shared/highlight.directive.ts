d67e019832dabc09360fa112e20cb0952bca38a5
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-06 12:28:14 +0100

    Lifecycle: constructor, OnChanges, OnInit, DoCheck

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -1,20 +1,33 @@
-import { Directive, ElementRef, Attribute, Input, Renderer2 } from "@angular/core";
+import { SimpleChanges } from '@angular/core';
+import {
+  Directive,
+  ElementRef
+  Input,
+  Renderer2,
+  OnChanges,
+  DoCheck
+} from "@angular/core";
 
 @Directive({
   selector: "[appHighlight]"
 })
-export class HighlightDirective {
-
+export class HighlightDirective implements OnChanges, DoCheck {
   @Input()
   appHighlight;
 
-  constructor(private elem: ElementRef, private renderer:Renderer2) {}
+  constructor(private elem: ElementRef, private renderer: Renderer2) {}
+
+  ngOnChanges(changes:SimpleChanges) {
+    console.log("ngOnChange",changes);
+    this.renderer.setStyle(this.elem.nativeElement, "color", this.appHighlight);
+  }
+  
+  ngDoCheck() {
+    console.log("ngDoCheck");
+  }
 
   ngOnInit() {
     console.log("hello", this.appHighlight);
 
-    this.renderer.setStyle(this.elem.nativeElement,'color',this.appHighlight)
-
-    // this.elem.nativeElement.style.color = this.appHighlight;
   }
 }
