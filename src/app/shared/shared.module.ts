3f9488dac1b73d1172437942e5591aef1baad8e9
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-06 15:15:12 +0100

    ng-content select='css selector'

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from "@angular/core";
 import { HighlightDirective } from "./highlight.directive";
 import { ValidateDirective } from "./validate.directive";
 import { UnlessDirective } from "./unless.directive";
+import { CardComponent } from './card/card.component';
 
 // ng g m shared -m playlists
 // ng g d shared/highlight -m shared --export true
 
 @NgModule({
-  declarations: [HighlightDirective, ValidateDirective, UnlessDirective],
-  exports: [HighlightDirective, ValidateDirective, UnlessDirective]
+  declarations: [HighlightDirective, ValidateDirective, UnlessDirective, CardComponent],
+  exports: [HighlightDirective, ValidateDirective, UnlessDirective, CardComponent]
 })
 export class SharedModule {}
