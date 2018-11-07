f30162c211a0e176653fa557094cf091719812ce
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-07 16:39:44 +0100

    nested dynamic forms

--- src/app/search/search-form/search-form.component.ts
+++ src/app/search/search-form/search-form.component.ts
@@ -13,16 +13,27 @@ import {
 })
 export class SearchFormComponent implements OnInit {
   queryForm: FormGroup;
+  markets: FormArray;
 
   constructor() {
     this.queryForm = new FormGroup({
-      query: new FormControl("batman",{
-        updateOn:'blur'
-      }),
-      type: new FormControl("album")
+      query: new FormControl("batman"),
+      options: new FormGroup({
+        type: new FormControl("album"),
+        markets: new FormArray([
+          new FormControl("PL"), 
+          new FormControl("UK")])
+      })
     });
+    this.markets = this.queryForm.get("options.markets") as FormArray;
 
-    console.log(this.queryForm)
+    console.log(this.queryForm);
+  }
+
+  addMarket() {
+    (this.queryForm.get("options.markets") as FormArray).push(
+      new FormControl("")
+    );
   }
 
   ngOnInit() {}
