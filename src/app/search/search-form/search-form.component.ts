08cffe8c8ad8266f9d4860b399b69db80ad05ad0
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-08 09:49:04 +0100

    rxmarbles.com - filter, distinctUntilChanges, debounceTime, throttleTime

--- src/app/search/search-form/search-form.component.ts
+++ src/app/search/search-form/search-form.component.ts
@@ -6,6 +6,12 @@ import {
   FormArray,
   FormBuilder
 } from "@angular/forms";
+import {
+  distinctUntilKeyChanged,
+  distinctUntilChanged,
+  filter,
+  debounceTime
+} from "rxjs/operators";
 
 @Component({
   selector: "app-search-form",
@@ -13,18 +19,21 @@ import {
   styleUrls: ["./search-form.component.css"]
 })
 export class SearchFormComponent implements OnInit {
-  queryForm: FormGroup;
-  markets: FormArray;
+  queryForm = new FormGroup({
+    query: new FormControl("batman")
+  });
 
   constructor() {
-    this.queryForm = new FormGroup({
-      query: new FormControl("batman")
-    });
-
-    this.queryForm.get("query")
-        .valueChanges.subscribe(query => {
-          this.search(query);
-        });
+    this.queryForm
+      .get("query")
+      .valueChanges.pipe(
+        debounceTime(400),
+        filter(query => query.length >= 3),
+        distinctUntilChanged(),
+      )
+      .subscribe(query => {
+        this.search(query);
+      });
 
     console.log(this.queryForm);
   }
