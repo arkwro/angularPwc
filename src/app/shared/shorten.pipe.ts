2ebf89784feead1fc442d637220fc697978156a9
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-09 10:05:53 +0100

    pure pipe

--- src/app/shared/shorten.pipe.ts
+++ src/app/shared/shorten.pipe.ts
@@ -2,14 +2,14 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
   name: "shorten",
-  pure: false
+  // pure: false
 })
 export class ShortenPipe implements PipeTransform {
   //
   // constructor() {}
 
   transform(value: any, length = 20): any {
-    return Math.random();
+    // return Math.random();
     // console.log('Now!')
 
     return value.length >= length ? value.substr(0, length) + "..." : value;
