a1d06b75975933461025590123df9656da556bd4
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-09 15:19:48 +0100

    testing events

--- src/app/testing/testing.component.spec.ts
+++ src/app/testing/testing.component.spec.ts
@@ -17,8 +17,8 @@ fdescribe("TestingComponent", () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TestingComponent);
-    instance = fixture.componentInstance
-    fixture.detectChanges()
+    instance = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it("should create", () => {
@@ -26,14 +26,32 @@ fdescribe("TestingComponent", () => {
   });
 
   it("should render message", () => {
-    expect(fixture.nativeElement.innerHTML).toMatch('testing works')
+    expect(fixture.nativeElement.innerHTML).toMatch("testing works");
   });
-  
-  it('should update when message change',()=>{
-    instance.message = 'Placki!'
-    fixture.detectChanges()
-    const elem = fixture.debugElement.query(By.css('.message'))
-    expect(elem.nativeElement.innerHTML).toMatch('Placki!')
-  })
 
+  it("should update when message change", () => {
+    instance.message = "Placki!";
+    fixture.detectChanges();
+    const elem = fixture.debugElement.query(By.css(".message"));
+    expect(elem.nativeElement.innerHTML).toMatch("Placki!");
+  });
+
+  it("should display message in input", () => {
+    const elem = fixture.debugElement.query(By.css("input"));
+
+    expect(elem.nativeElement.value).toMatch(instance.message);
+  });
+
+  it("should update message when input changes", () => {
+    const elem = fixture.debugElement.query(By.css("input"));
+
+    elem.nativeElement.value = "Placki";
+    elem.nativeElement.dispatchEvent(new Event("input"));
+    // or:
+    elem.triggerEventHandler("input", {
+      target: { value: "Placki" }
+    });
+
+    expect(instance.message).toMatch("Placki");
+  });
 });
