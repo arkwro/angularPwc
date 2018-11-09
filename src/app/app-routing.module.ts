27da7881247fda1f022ac536a8b3d3d30ba40549
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-09 10:23:54 +0100

    RouterModule, useHash, Routes, redirectTo, pathMach, CatchAll **

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { PlaylistsViewComponent } from "./playlists/playlists-view/playlists-view.component";
+import { MusicSearchComponent } from "./search/music-search/music-search.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "playlists",
+    pathMatch: "full" //'prefix'
+  },
+  {
+    path: "playlists",
+    component: PlaylistsViewComponent
+  },
+  {
+    path: "search",
+    component: MusicSearchComponent
+  },
+  {
+    path: "**",
+    redirectTo: "search",
+    pathMatch: "full" //'prefix'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {})],
+  imports: [
+    RouterModule.forRoot(routes, {
+      enableTracing: true,
+      // onSameUrlNavigation:'ignore',
+      // paramsInheritanceStrategy:'always',
+      useHash: true
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
