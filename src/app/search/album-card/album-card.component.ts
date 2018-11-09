a56c74a090f32c43504e563b9d155200fafdf2f3
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-09 09:55:26 +0100

    changeDetection:ChangeDetectionStrategy.OnPush, immutable Input values

--- src/app/search/album-card/album-card.component.ts
+++ src/app/search/album-card/album-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, HostBinding, Input } from "@angular/core";
+import { Component, OnInit, HostBinding, Input, ChangeDetectionStrategy } from "@angular/core";
 import { Album } from "src/app/models/album";
 
 @Component({
   selector: "app-album-card, [app-album-card]",
   templateUrl: "./album-card.component.html",
-  styleUrls: ["./album-card.component.css"]
+  styleUrls: ["./album-card.component.css"],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class AlbumCardComponent implements OnInit {
 
--- src/app/search/albums-grid/albums-grid.component.ts
+++ src/app/search/albums-grid/albums-grid.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Album } from 'src/app/models/album';
 
 @Component({
   selector: 'app-albums-grid',
   templateUrl: './albums-grid.component.html',
-  styleUrls: ['./albums-grid.component.css']
+  styleUrls: ['./albums-grid.component.css'],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class AlbumsGridComponent implements OnInit {
 
